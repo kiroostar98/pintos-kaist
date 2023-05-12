27043774a3fcfad257043833efa0b856834d4365
Ubuntu <ubuntu@ip-172-31-47-217.ap-northeast-2.compute.internal>
2023-05-12 07:05:09 +0000

    Memory Management

--- vm/vm.c
+++ vm/vm.c
@@ -3,6 +3,10 @@
 #include "threads/malloc.h"
 #include "vm/vm.h"
 #include "vm/inspect.h"
+#include "hash.h"
+
+/* frame 관리 테이블 */
+struct list frame_table;
 
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
@@ -63,20 +67,41 @@ err:
 /* Find VA from spt and return page. On error, return NULL. */
 struct page *
 spt_find_page (struct supplemental_page_table *spt UNUSED, void *va UNUSED) {
-	struct page *page = NULL;
-	/* TODO: Fill this function. */
+	// 인자로 받은 spt 내에서 va를 키로 전달해서 이를 갖는 page를 리턴한다.
+	struct page *page = (struct page *)malloc(sizeof(struct page));
+	struct hash_elem *e;
 
-	return page;
+	/*
+	hash_find(): hash_elem을 리턴해준다. 이로부터 우리는 해당 page를 찾을 수 있어.
+	해당 spt의 hash 테이블 구조체를 인자로 넣자. 해당 해시 테이블에서 찾아야 하니까.
+	근데 우리가 받은 건 va 뿐이다. 근데 hash_find()는 hash_elem을 인자로 받아야 하니
+	dummy page 하나를 만들고 그것의 가상주소를 va로 만들어. 
+	-> 그 다음 이 페이지의 hash_elem(=spt_hash_elem)을 넣는다.
+	*/
+
+	page->va = pg_round_down(va);
+	e = hash_find(&spt->spt_hash, &page->spt_hash_elem);
+	free(page);
+
+	// hash_entry(HASH_ELEM, STRUCT, MEMBER): 매크로는 HASH_ELEM이 STRUCT 안에 포함된 MEMBER 
+	// 멤버 변수로 링크되어 있을 때, 그 멤버변수의 값을 포함하는 STRUCT 구조체의 포인터를 반환하는 매크로
+	return e != NULL ? hash_entry(e, struct page, spt_hash_elem) : NULL;
 }
 
 /* Insert PAGE into spt with validation. */
 bool
-spt_insert_page (struct supplemental_page_table *spt UNUSED,
-		struct page *page UNUSED) {
-	int succ = false;
-	/* TODO: Fill this function. */
-
-	return succ;
+spt_insert_page (struct supplemental_page_table *spt UNUSED, struct page *page UNUSED) {
+	// 인자로 주어진 struct page를 spt에 삽입
+	// hash_insert(): 
+	// 새로운 데이터와 같은 데이터가 이미 해시 테이블에 존재하는 경우, 해당 데이터의 포인터가 반환 -> 삽입되지 않음
+	// 같은 데이터가 해시 테이블에 존재하지 않는 경우, NULL 포인터가 반환 -> 새로운 데이터가 해시 테이블에 삽입되었음
+	if (!hash_insert(&spt->spt_hash, &page->spt_hash_elem)){
+		// 해빈 - page_insert로 바꿔야할지도
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 void
@@ -89,7 +114,9 @@ spt_remove_page (struct supplemental_page_table *spt, struct page *page) {
 static struct frame *
 vm_get_victim (void) {
 	struct frame *victim = NULL;
-	 /* TODO: The policy for eviction is up to you. */
+	/* TODO: The policy for eviction is up to you. */
+	// 구현해야함
+	// for문을 돌면서 pml4에서 제거할 애를 찾는다. 
 
 	return victim;
 }
@@ -100,7 +127,9 @@ static struct frame *
 vm_evict_frame (void) {
 	struct frame *victim UNUSED = vm_get_victim ();
 	/* TODO: swap out the victim and return the evicted frame. */
-
+	// 구현해야함
+	// 이 함수는 page에 달려있는 frame 공간을 디스크로 내리는 swap out을 진행하는 함수이다. 
+	// swap_out()은 매크로로 구현되어 있으니 이를 적용해주면 된다.
 	return NULL;
 }
 
@@ -110,11 +139,16 @@ vm_evict_frame (void) {
  * space.*/
 static struct frame *
 vm_get_frame (void) {
-	struct frame *frame = NULL;
-	/* TODO: Fill this function. */
-
+	// 가상 메모리 프레임을 가져오는 함수
+	// palloc()을 호출하고 프레임을 가져옵니다. 사용 가능한 페이지가 없으면 페이지를 퇴거하고 를 반환합니다. 
+	// 이 함수는 항상 유효한 주소를 반환합니다. 즉, 사용자 풀 메모리가 가득 차면 이 함수는 프레임을 퇴거시켜 사용 가능한 메모리 공간을 확보합니다
+	struct frame *frame = (struct frame *)malloc(sizeof(struct frame));
+	// 해빈- palloc으로 만들었었음
 	ASSERT (frame != NULL);
 	ASSERT (frame->page == NULL);
+	list_push_back(&frame_table, &frame->frame_elem);
+	frame->page = NULL;
+
 	return frame;
 }
 
@@ -151,9 +185,12 @@ vm_dealloc_page (struct page *page) {
 /* Claim the page that allocate on VA. */
 bool
 vm_claim_page (void *va UNUSED) {
+	// 현재 스레드의 spt에서 va를 통해 page를 찾은뒤 null이면 false리턴
 	struct page *page = NULL;
-	/* TODO: Fill this function */
-
+	page = spt_find_page(&thread_current()->spt, va);
+	if (page == NULL)
+		return false;
+	
 	return vm_do_claim_page (page);
 }
 
@@ -166,14 +203,20 @@ vm_do_claim_page (struct page *page) {
 	frame->page = page;
 	page->frame = frame;
 
-	/* TODO: Insert page table entry to map page's VA to frame's PA. */
-
-	return swap_in (page, frame->kva);
+	// install_page(): 페이지의 va를 프레임의 kav에 매핑한다. 성공 : true
+	if (install_page(page->va, frame->kva, page->writable)){
+		return swap_in (page, frame->kva);
+	}
+	return false;
 }
 
 /* Initialize new supplemental page table */
 void
 supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) {
+	// 구현
+	// 이 함수는 새로운 프로세스가 시작할 때 호출되거나 (initd in userprog/process.c )
+	// 프로세스가 fork될 때 호출된다.(__do_fork in userprog.process.c)
+	hash_init(&spt->spt_hash, page_hash, page_less, NULL);
 }
 
 /* Copy supplemental page table from src to dst */
@@ -187,4 +230,21 @@ void
 supplemental_page_table_kill (struct supplemental_page_table *spt UNUSED) {
 	/* TODO: Destroy all the supplemental_page_table hold by thread and
 	 * TODO: writeback all the modified contents to the storage. */
+}
+
+// 추가
+/* Returns a hash value for page p. */
+unsigned
+page_hash (const struct hash_elem *p_, void *aux UNUSED) {
+  const struct page *p = hash_entry (p_, struct page, spt_hash_elem);
+  return hash_bytes (&p->va, sizeof p->va);
+}
+
+/* Returns true if page a precedes page b. */
+bool
+page_less (const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED) {
+  const struct page *a = hash_entry (a_, struct page, spt_hash_elem);
+  const struct page *b = hash_entry (b_, struct page, spt_hash_elem);
+
+  return a->va < b->va;
 }
